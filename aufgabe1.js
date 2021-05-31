a873cd4f45901c7a13e1cbd896b1227a35a3cfe8
Gulsenem <og.durdyeva@gmail.com>
2021-05-31 23:54:43 +0200

    gut

--- aufgabe1.js
+++ aufgabe1.js
@@ -2,22 +2,39 @@ var zahl1 = document.getElementById("zahl1");
 var zahl2 = document.getElementById("zahl2");
 var rech;
 var erg = document.getElementById("ergebnis");
+var rech_btn = document.getElementById("rech_btn");
 
 
+var is_rech_btn = true;
 
 function rechnen()
 {
-    if ( zahl1.value != "" && zahl2.value != "" )
+    if (is_rech_btn)
     {
-        rech = parseFloat(zahl1.value) + parseFloat(zahl2.value); 
-        erg.innerHTML = rech;       
+        rech_btn.innerHTML = "Delete";
+        is_rech_btn = false;
+        if ( zahl1.value != "" && zahl2.value != "" )
+        {
+            rech = parseFloat(zahl1.value) + parseFloat(zahl2.value); 
+            erg.innerHTML = rech;       
+        }
+        else if ( zahl1.value == "" && zahl2.value == "" )
+        {
+            erg.innerHTML = "Zahl ist nicht angegeben worden";
+            erg.style.color = "red"; 
+            erg.style.fontSize = "16px";
+        }
     }
-    else if ( zahl1.value == "" && zahl2.value == "" )
+    else if (!is_rech_btn)
     {
-        erg.innerHTML = "Zahl ist nicht angegeben worden";
-        erg.style.color = "red"; 
-        erg.style.fontSize = "16px";
-    }
+        zahl1.value = "" ;
+        zahl2.value = "";
+        erg.innerHTML = ""; 
 
+        rech_btn.innerHTML = "Rechnen";
+        is_rech_btn = true;
+
+
+    }
 }
 
